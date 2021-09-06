587eb7b2fb936c9819e1ddfa8b4df931a3b10dc3
Ali <alicemalturan1@gmail.com>
2021-09-06 05:18:57 +0300

    panel edited

---
+++ app/Models/TopMenuItems.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TopMenuItems extends Model
+{
+    use HasFactory;
+    protected $table='topmenu_items';
+}
